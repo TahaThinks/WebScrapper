fee88743e6a8dc91226bb810987bc9e1b66c587d
TahaThinks <tahaa4587@gmail.com>
2024-05-29 21:18:37 +0400

    get element of a certain class

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ with open("website.html", encoding="utf8") as file:
 soup = BeautifulSoup(contents, 'html.parser')
 
 all_anchor_tags = soup.find_all(name='a')
-heading = soup.find(name="h1", id="name")
-print(heading)
 
 for tag in all_anchor_tags:
     # print(tag.getText())
     print(tag.get("href"))
 
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
