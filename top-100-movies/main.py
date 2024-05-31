52d8b79f415ba777b4cb179b8f314193cd79654a
TahaThinks <tahaa4587@gmail.com>
2024-05-31 21:35:25 +0400

    Scrap Web Page

--- top-100-movies/main.py
+++ top-100-movies/main.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+response = requests.get(url)
+movies_webpage = response.text
+
+soup = BeautifulSoup(movies_webpage, "html.parser")
+movies_list = [movie.text for movie in soup.find_all(name="h3", class_="title")]
+print(movies_list[::-1])
