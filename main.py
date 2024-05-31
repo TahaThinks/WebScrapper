6b87c7c8bc3c4fb2767d62d6a82b9af303b94205
TahaThinks <tahaa4587@gmail.com>
2024-05-31 11:23:08 +0400

    Get a List of all the links

--- main.py
+++ main.py
@@ -2,5 +2,12 @@
 from bs4 import BeautifulSoup
 import requests
 
+response = requests.get("https://news.ycombinator.com/news")
+yc_webpage = response.text
 
+soup = BeautifulSoup(yc_webpage, "html.parser")
+links = soup.find(name='span', class_="titleline")
 
+# print(link.a["href"])
+# for Links the href is in an anchor tag with span class="titleline"
+# Score is in Class="Score"
