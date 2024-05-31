6cebb9986bb2cf97b014f4558cf7962c09ed8507
TahaThinks <tahaa4587@gmail.com>
2024-05-31 11:33:27 +0400

    Get title, link and count

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
-links = soup.find(name='span', class_="titleline")
 
-# print(link.a["href"])
-# for Links the href is in an anchor tag with span class="titleline"
-# Score is in Class="Score"
+article_tag = soup.find(name="span", class_="titleline")
+print(article_tag.a.text)
+print(article_tag.a['href'])
+
+article_score = soup.find(name="span", class_="score")
+print(article_score.text.split()[0])
+
+
+# article_links = soup.find_all(name='span', class_="titleline")
+
+# for article_link in article_links:
+#     print(article_link.a.text)
