3847eea7ba63fd24cf6e74a48cee33a8e643523d
TahaThinks <tahaa4587@gmail.com>
2024-05-31 11:47:30 +0400

    Change the counts to int

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ for article_tag in articles_tags:
     article_texts.append(article_tag.a.text)
     article_links.append(article_tag.a['href'])
 
-articles_upvotes = [score.text for score in soup.find_all(name="span", class_="score")]
+articles_upvotes = [int(score.text.split()[0]) for score in soup.find_all(name="span", class_="score")]
 
 print(article_texts)
 print(article_links)
